8b50963088fcb4147ff22042b1eaa11bbac09bcf
metlosz <53083106+metlosz@users.noreply.github.com>
2024-05-12 08:37:10 +0200

    Update EU_BIWLP.py

--- asintf/EU_BIWLP.py
+++ asintf/EU_BIWLP.py
@@ -15,7 +15,7 @@ class EU_BIWLP(EU_IWLP):
     References
     ----------
     [1] M. Guzik and K. Kowalczyk, "On Ambisonic Source Separation with Spatially Informed Non-negative Tensor
-    Factorization". (to be completed after publication)
+    Factorization," in IEEE/ACM Transactions on Audio, Speech, and Language Processing, doi: 10.1109/TASLP.2024.3399618.
 
     Notes
     -----
